6b20ab67e752b1b97d91de457956e1431994ca0d
IDCreativ <contact@id-creativ.com>
2022-04-02 12:44:16 +0200

    add construction and language switcher

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -12,15 +12,30 @@ class AppController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(TranslatorInterface $translator): Response
+    public function home(TranslatorInterface $translator): Response
+    {
+        // Test de traduction dans un controller
+        $controller_name = $translator->trans('menu.home');
+        // $message_flash = $translator->trans('message.flash.test');
+
+        // $this->addFlash('success', $message_flash);
+        return $this->render('app/index.html.twig', [
+            'controller_name' => $controller_name,
+        ]);
+    }
+
+    /**
+     * @Route("/modeles", name="modeles")
+     */
+    public function modeles(TranslatorInterface $translator): Response
     {
         // Test de traduction dans un controller
         $controller_name = $translator->trans('menu.home');
         $message_flash = $translator->trans('message.flash.test');
 
         $this->addFlash('success', $message_flash);
-        return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+        return $this->render('__modeles/index.html.twig', [
+            'controller_name' => 'Modèles',
         ]);
     }
 }
